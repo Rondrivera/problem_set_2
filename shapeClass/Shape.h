46ed5f49babe382e1ee933101b6df16de8ba7d2b
Ron Rivera <Ron.drivera@gmail.com>
2015-08-09 23:48:16 -0400

    first commit

---
+++ shapeClass/Shape.h
@@ -0,0 +1,13 @@
+//
+//  Shape.h
+//  shapeClass
+//
+//  Created by Ronald Rivera on 8/8/15.
+//  Copyright (c) 2015 Ronald Rivera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Shape : NSObject
+
+@end
---
+++ shapeClass/Shape.m
@@ -0,0 +1,13 @@
+//
+//  Shape.m
+//  shapeClass
+//
+//  Created by Ronald Rivera on 8/8/15.
+//  Copyright (c) 2015 Ronald Rivera. All rights reserved.
+//
+
+#import "Shape.h"
+
+@implementation Shape
+
+@end
